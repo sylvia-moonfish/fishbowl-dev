dfdaafb3480057eef80722c3f4943718f2abc864
sylvia-moonfish <46030655+sylvia-moonfish@users.noreply.github.com>
2021-05-12 01:17:40 -0700

    test

--- pages/bis-guide/[version]/[job].js
+++ pages/bis-guide/[version]/[job].js
@@ -1,7 +1,14 @@
+import AppBar from "@material-ui/core/AppBar";
+import Avatar from "@material-ui/core/Avatar";
 import Breadcrumbs from "@material-ui/core/Breadcrumbs";
+import Chip from "@material-ui/core/Chip";
 import Container from "@material-ui/core/Container";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
 import Grid from "@material-ui/core/Grid";
+import Grow from "@material-ui/core/Grow";
 import Hidden from "@material-ui/core/Hidden";
+import IconButton from "@material-ui/core/IconButton";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import Link from "@material-ui/core/Link";
 import { makeStyles } from "@material-ui/core/styles";
@@ -12,7 +19,10 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Toolbar from "@material-ui/core/Toolbar";
+import Tooltip from "@material-ui/core/Tooltip";
 import Typography from "@material-ui/core/Typography";
+
+import CloseIcon from "@material-ui/icons/Close";
 
 import { useRouter } from "next/router";
 import React from "react";
@@ -29,10 +39,16 @@ const useStyles = makeStyles((theme) => ({
     maxWidth: 128,
     width: "100%",
   },
+  grow: {
+    flexGrow: 1,
+  },
   link: {
     color: "inherit",
     cursor: "pointer",
     textDecoration: "none",
+  },
+  noMargin: {
+    margin: 0,
   },
 }));
 
@@ -91,12 +107,21 @@ export async function getStaticProps(context) {
   };
 }
 
+const transition = React.forwardRef((props, ref) => {
+  return <Grow {...props} ref={ref} />;
+});
+
 const BisGuideJob = (props) => {
   const router = useRouter();
 
   if (router.isFallback) {
     return <LinearProgress color="secondary" />;
   }
+
+  const { version, job } = router.query;
+
+  const [selectedTableIndex, setSelectedTableIndex] = React.useState(-1);
+  const [selectedGearSetIndex, setSelectedGearSetIndex] = React.useState(-1);
 
   const classes = useStyles();
 
@@ -192,26 +217,261 @@ const BisGuideJob = (props) => {
                   <TableBody>
                     {table.gearSets.map((gearSet, gearSetIndex) => {
                       return (
-                        <TableRow
-                          className={classes.link}
-                          hover
-                          key={gearSetIndex}
-                          onClick={() => {}}
-                        >
-                          <Hidden mdDown>
-                            <TableCell>{gearSet.title}</TableCell>
-                          </Hidden>
-                          {table.columnDefinitions.map(
-                            (columnDefinition, columnDefinitionIndex) => {
-                              return (
-                                <TableCell key={columnDefinitionIndex}>
-                                  {gearSet[columnDefinition.propertyName]}
-                                  {columnDefinition.append}
-                                </TableCell>
-                              );
+                        <React.Fragment key={gearSetIndex}>
+                          <TableRow
+                            className={classes.link}
+                            hover
+                            onClick={() => {
+                              setSelectedTableIndex(tableIndex);
+                              setSelectedGearSetIndex(gearSetIndex);
+                            }}
+                          >
+                            <Hidden mdDown>
+                              <TableCell>{gearSet.title}</TableCell>
+                            </Hidden>
+                            {table.columnDefinitions.map(
+                              (columnDefinition, columnDefinitionIndex) => {
+                                return (
+                                  <TableCell key={columnDefinitionIndex}>
+                                    {gearSet[columnDefinition.propertyName]}
+                                    {columnDefinition.append}
+                                  </TableCell>
+                                );
+                              }
+                            )}
+                          </TableRow>
+                          <Dialog
+                            fullWidth
+                            maxWidth="lg"
+                            onClose={() => {
+                              setSelectedTableIndex(-1);
+                              setSelectedGearSetIndex(-1);
+                            }}
+                            open={
+                              selectedTableIndex === tableIndex &&
+                              selectedGearSetIndex === gearSetIndex
                             }
-                          )}
-                        </TableRow>
+                            scroll="paper"
+                            TransitionComponent={transition}
+                          >
+                            <AppBar position="static">
+                              <Toolbar variant="dense">
+                                <div className={classes.grow}>
+                                  <Typography variant="body1">
+                                    {gearSet.title}
+                                  </Typography>
+                                </div>
+                                <IconButton
+                                  onClick={() => {
+                                    setSelectedTableIndex(-1);
+                                    setSelectedGearSetIndex(-1);
+                                  }}
+                                >
+                                  <CloseIcon />
+                                </IconButton>
+                              </Toolbar>
+                            </AppBar>
+                            <DialogContent>
+                              <Grid container direction="column" spacing={5}>
+                                <Grid item />
+                                <Grid item>
+                                  <li>{gearSet.description}</li>
+                                </Grid>
+                                <Grid item>
+                                  <TableContainer>
+                                    <Table size="small">
+                                      <TableHead>
+                                        <TableRow>
+                                          {table.statDefinitions.map(
+                                            (
+                                              statDefinition,
+                                              statDefinitionIndex
+                                            ) => {
+                                              return (
+                                                <TableCell
+                                                  align="center"
+                                                  key={statDefinitionIndex}
+                                                >
+                                                  {statDefinition.printedName}
+                                                </TableCell>
+                                              );
+                                            }
+                                          )}
+                                        </TableRow>
+                                      </TableHead>
+                                      <TableBody>
+                                        <TableRow>
+                                          {table.statDefinitions.map(
+                                            (
+                                              statDefinition,
+                                              statDefinitionIndex
+                                            ) => {
+                                              return (
+                                                <TableCell
+                                                  align="center"
+                                                  key={statDefinitionIndex}
+                                                >
+                                                  {
+                                                    gearSet.attribute[
+                                                      statDefinition
+                                                        .propertyName
+                                                    ]
+                                                  }
+                                                </TableCell>
+                                              );
+                                            }
+                                          )}
+                                        </TableRow>
+                                      </TableBody>
+                                    </Table>
+                                  </TableContainer>
+                                </Grid>
+                                <Grid item>
+                                  <TableContainer>
+                                    <Table size="small">
+                                      <TableHead>
+                                        <TableRow>
+                                          <TableCell colSpan={2} />
+                                          <TableCell align="center">
+                                            종류
+                                          </TableCell>
+                                          <TableCell align="center">
+                                            이름
+                                          </TableCell>
+                                          <TableCell align="center">
+                                            마테리아
+                                          </TableCell>
+                                          <TableCell align="center">
+                                            필요 재화
+                                          </TableCell>
+                                        </TableRow>
+                                      </TableHead>
+                                      <TableBody>
+                                        <TableRow>
+                                          <TableCell>음식</TableCell>
+                                          <TableCell>
+                                            <Avatar
+                                              src={gearSet.meal.iconSrc}
+                                              variant="square"
+                                            />
+                                          </TableCell>
+                                          <TableCell>제작</TableCell>
+                                          <TableCell>
+                                            {gearSet.meal.nameKo
+                                              ? gearSet.meal.nameKo
+                                              : gearSet.meal.nameEn}
+                                          </TableCell>
+                                          <TableCell colSpan={3} />
+                                        </TableRow>
+                                        {gearSet.gears.map(
+                                          (gear, gearIndex) => {
+                                            return (
+                                              <TableRow
+                                                key={gearIndex}
+                                                selected={
+                                                  gear.from === "영웅 레이드" ||
+                                                  gear.from === "극만신" ||
+                                                  gear.from === "일반 레이드"
+                                                }
+                                              >
+                                                <TableCell>
+                                                  {gear.type}
+                                                </TableCell>
+                                                <TableCell>
+                                                  <Avatar
+                                                    src={gear.iconSrc}
+                                                    variant="square"
+                                                  />
+                                                </TableCell>
+                                                <TableCell>
+                                                  {gear.from}
+                                                </TableCell>
+                                                <TableCell>
+                                                  {gear.nameKo
+                                                    ? gear.nameKo
+                                                    : gear.nameEn}
+                                                </TableCell>
+                                                {gear.materias.length === 0 ? (
+                                                  <TableCell />
+                                                ) : (
+                                                  <TableCell>
+                                                    <Grid
+                                                      container
+                                                      direction="row"
+                                                      spacing={1}
+                                                    >
+                                                      {gear.materias.map(
+                                                        (
+                                                          materia,
+                                                          materiaIndex
+                                                        ) => {
+                                                          return (
+                                                            <Grid
+                                                              item
+                                                              key={materiaIndex}
+                                                            >
+                                                              <Tooltip
+                                                                title={
+                                                                  materia.nameKo
+                                                                    ? materia.nameKo
+                                                                    : materia.nameEn
+                                                                }
+                                                              >
+                                                                <Chip
+                                                                  avatar={
+                                                                    <Avatar
+                                                                      src={`/materia-slots/${
+                                                                        materiaIndex <
+                                                                        gear.materiaSlots
+                                                                          ? "blue"
+                                                                          : "red"
+                                                                      }${
+                                                                        materia.materiaNumber
+                                                                      }.png`}
+                                                                    />
+                                                                  }
+                                                                  label={
+                                                                    materia.shortName
+                                                                  }
+                                                                  size="small"
+                                                                  variant="outlined"
+                                                                />
+                                                              </Tooltip>
+                                                            </Grid>
+                                                          );
+                                                        }
+                                                      )}
+                                                    </Grid>
+                                                  </TableCell>
+                                                )}
+                                                <TableCell>
+                                                  {gear.required
+                                                    .split("\n")
+                                                    .map((str, strIndex) => {
+                                                      return (
+                                                        <p
+                                                          className={
+                                                            classes.noMargin
+                                                          }
+                                                          key={strIndex}
+                                                        >
+                                                          {str}
+                                                        </p>
+                                                      );
+                                                    })}
+                                                </TableCell>
+                                              </TableRow>
+                                            );
+                                          }
+                                        )}
+                                      </TableBody>
+                                    </Table>
+                                  </TableContainer>
+                                </Grid>
+                              </Grid>
+                            </DialogContent>
+                          </Dialog>
+                        </React.Fragment>
                       );
                     })}
                   </TableBody>
